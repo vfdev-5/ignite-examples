8fedc6bc9a9650d294d7458c2f4a3926bb6f23dd
vfdev-5 <vfdev.5@gmail.com>
2018-05-03 22:37:05 +0200

    Another train config for inception resnet

--- classification/imaterialist_challenge_furniture_2018/configs/train_inceptionresnetv2_350_smart_sampler_resized_crop_updated.py
+++ classification/imaterialist_challenge_furniture_2018/configs/train_inceptionresnetv2_350_smart_sampler_resized_crop_updated.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import numpy as np
 import torch
-from torch.optim import Adam
+from torch.optim import SGD
 from torch.optim.lr_scheduler import ReduceLROnPlateau, MultiStepLR
 from torchvision.transforms import RandomHorizontalFlip, RandomVerticalFlip
 from torchvision.transforms import RandomResizedCrop
@@ -11,33 +11,34 @@ from ignite.engines import Events
 from common.dataset import FilesFromCsvDataset
 from common.data_loaders import get_data_loader
 from common.sampling import SmartWeightedRandomSampler
-from models.squeezenet_350 import FurnitureSqueezeNet350
+from models.inceptionresnetv2 import FurnitureInceptionResNet299
 
 
-SEED = 17
+SEED = 12345
 DEBUG = True
 
 OUTPUT_PATH = "output"
+DATASET_PATH = Path("/home/fast_storage/imaterialist-challenge-furniture-2018/")
 
 size = 350
 
 TRAIN_TRANSFORMS = [
-    RandomResizedCrop(size, scale=(0.7, 1.0)),
+    RandomResizedCrop(size, scale=(0.7, 1.0), interpolation=3),
     RandomHorizontalFlip(p=0.5),
-    ColorJitter(hue=0.1, brightness=0.2, contrast=0.2),
+    ColorJitter(hue=0.12, brightness=0.12),
     ToTensor(),
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ]
 
 VAL_TRANSFORMS = [
-    RandomResizedCrop(size, scale=(0.8, 1.0)),
+    RandomResizedCrop(size, scale=(0.7, 1.0), interpolation=3),
     RandomHorizontalFlip(p=0.5),
     ToTensor(),
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ]
 
 
-BATCH_SIZE = 180
+BATCH_SIZE = 24
 NUM_WORKERS = 15
 
 
@@ -59,21 +60,25 @@ VAL_LOADER = get_data_loader(val_dataset,
                              cuda=True)
 
 
-MODEL = FurnitureSqueezeNet350(pretrained=True)
+MODEL = FurnitureInceptionResNet299(pretrained='imagenet')
 
 N_EPOCHS = 100
 
-OPTIM = Adam(
+OPTIM = SGD(
     params=[
-        {"params": MODEL.features.parameters(), 'lr': 0.0001},
-        {"params": MODEL.classifier.parameters(), 'lr': 0.001},
+        {"params": MODEL.stem.parameters(), 'lr': 0.0001},
+        {"params": MODEL.features.parameters(), 'lr': 0.002},
+        {"params": MODEL.classifier.parameters(), 'lr': 0.1},
     ],
-)
+    momentum=0.9)
+
 
 LR_SCHEDULERS = [
-    MultiStepLR(OPTIM, milestones=[4, 5, 7, 9, 10, 11, 12, 13], gamma=0.55)
+    MultiStepLR(OPTIM, milestones=[4, 8, 12], gamma=0.1)
 ]
 
+
+REDUCE_LR_ON_PLATEAU = ReduceLROnPlateau(OPTIM, mode='min', factor=0.5, patience=3, threshold=0.08, verbose=True)
 
 EARLY_STOPPING_KWARGS = {
     'patience': 15,
@@ -82,6 +87,7 @@ EARLY_STOPPING_KWARGS = {
 
 LOG_INTERVAL = 100
 
+
 TRAINER_CUSTOM_EVENT_HANDLERS = [
     # (event, handler), handler signature should be `foo(trainer, evaluator, logger)`
 
@@ -89,10 +95,10 @@ TRAINER_CUSTOM_EVENT_HANDLERS = [
 
 
 def smart_sampling_update(evaluator, trainer, logger):
-
-    if trainer.state.epoch < 4:
+    
+    if trainer.state.epoch < 5:
         return
-
+    
     assert hasattr(evaluator.state, "metrics"), "Evaluator state has no metrics"
     recall_per_class = evaluator.state.metrics['recall'].cpu()
     mean_recall_per_class = torch.mean(recall_per_class)
@@ -102,7 +108,7 @@ def smart_sampling_update(evaluator, trainer, logger):
     n_classes = len(recall_per_class)
     class_weights = []
     for c in range(n_classes):
-        w = 2.0 if c in low_recall_classes else 1.0
+        w = 5.0 if c in low_recall_classes else 1.0
         class_weights.append((c, w))
     train_sampler.update_weights(class_weights)
 
